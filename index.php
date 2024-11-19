a6b9fa5348d1226aefb851fb54411f30e22fe4c8
Arno <r0893222@student.thomasmore.be>
2024-11-19 22:11:48 +0100

    new update

--- index.php
+++ index.php
@@ -11,25 +11,81 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/newIndex.css">
+    <title>Webshop</title>
 </head>
 <body>
 
 <nav class="navbar">
     <a href="index.php" class="logo">HENK</a>
-        <a href="#">Eetkamerstoelen</a>
-        <a href="#">Zitbanken</a>
-        <a href="#">Fauteuils</a>
-        <a href="#">Poefs</a>
-    
-    
-    <form action="" method="get">
-      <input placeholder="search" type="text" name="search">
-    </form>
+        <a href="products.php">All products</a>
+        <a href="#">Dinning room chairs</a>
+        <a href="#">Sofas</a>
+        <a href="#">Armchairs</a>
+        <a href="#">Poufs</a>
     
     <a href="login.php" class="logout" ><p></p>Logout</a>
 </nav>
+
+<main>
+
+<section class="promo_video">
+  <div class="video">
+      <video muted loop playsinline autoplay src="https://assets.studio-henk.nl/assets/Images/2024-products/aw24/StudioHenk_AW24_brandvideo1_1920x1080_website.mp4" type="video/mp4"></video>
+  </div>
+
+</section>
+
+<section class="our_products">
+  <div class="our_products_header">
+    <h2>Our Products</h2>
+  </div>
+
+  <div class="products">
+    <div class="product">
+      <a href="#">
+        <img  src="Images/product_drc.webp" alt="">
+        <div class="item__content">
+          <p class="item__title">Dinning room chairs</p>
+        </div>
+      </a>
+    </div>
+
+    <div class="product">
+      <a href="#">
+        <img  src="Images/product_sofa.jpg" alt="">
+        <div class="item__content">
+          <p class="item__title">Sofas</p>
+        </div>
+      </a>
+    </div>
+
+    <div class="product">
+      <a href="#">
+        <img  src="Images/product_armchair.jpg" alt="">
+        <div class="item__content">
+          <p class="item__title">Armchairs</p>
+        </div>
+      </a>
+    </div>
+
+    <div class="product">
+      <a href="#">
+        <img  src="Images/product_pouf.jpg" alt="">
+        <div class="item__content">
+          <p class="item__title">Poufs</p>
+        </div>
+      </a>
+    </div>
+
+  </div>
+
+  
+
+</section>
+  
+
+</main>
 
 
     
