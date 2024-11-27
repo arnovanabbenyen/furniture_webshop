ecbf343610cbb161b4d3479057505de19d977873
Arno <r0893222@student.thomasmore.be>
2024-11-27 12:10:21 +0100

    test: db connection through class

--- test.php
+++ test.php
@@ -1,15 +1,5 @@
 <?php
-  $pathToSSL = __DIR__ . '/cacert.pem';
-  $options = array(PDO::MYSQL_ATTR_SSL_CA => $pathToSSL);
-
-  $host = 'studiohenkfurniture.mysql.database.azure.com';
-  $db = 'henk';
-  $user = 'Arno';
-  $pass = '$2y$12$1D3KI6THZZzYSXw6SjFz6.OB/Hn/JbgK/rnIbsZKzKgPgxYJX21LK';
-  $db = new PDO("mysql:host=$host;dbname=$db", $user, $pass, $options);
-
-  $users = $db->query('SELECT * FROM user');
-  var_dump($users->fetchAll());
+  include_once(__DIR__ . "/classes/Db.php");
 
 
 ?><!DOCTYPE html>
